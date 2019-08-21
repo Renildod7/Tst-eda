44f215967c46aa1c4d79c94e480dd9e2a6d6e348
Renildod7 <renildo.melo@ccc.ufcg.edu.br>
2019-08-21 12:59:38 -0300

    Selection Sort Passo a Passo.

---
+++ SelectionSortPassoAPasso/src/SelectionSortPassoAPasso.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class SelectionSortPassoAPasso {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] sec = sc.nextLine().split(" ");
+		int[] numSec = toArrayInt(sec);
+		selectionSortPassoAPasso(numSec);
+		sc.close();
+	}
+	
+	private static void selectionSortPassoAPasso(int[] v ) {
+		for(int i = 0; i < v.length - 1; i++) {
+			int iMenor = i;
+			boolean houveTroca = false;
+			for(int j = i+1; j < v.length; j++) {
+				if(v[j] < v[iMenor]) {
+					iMenor = j;
+					houveTroca = true;
+					
+				}
+			}
+			if(houveTroca) {
+				int aux = v[i];
+				v[i] = v[iMenor];
+				v[iMenor] = aux;
+				System.out.println(Arrays.toString(v));				
+			}
+		}
+		
+	}
+	
+	private static int[] toArrayInt(String[] sec) {
+		int[] numSec = new int[sec.length];
+		for(int i = 0; i < sec.length; i++) {
+			numSec[i] = Integer.parseInt(sec[i]);
+		}
+		return numSec;
+	}
+}
