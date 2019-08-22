7126855b09f13b06ebbf0853cbba907c56d48991
Renildod7 <renildo.melo@ccc.ufcg.edu.br>
2019-08-21 21:16:20 -0300

    Verificação de lementos duplicados

---
+++ VerificaçãoDeElementosDuplicados/VerificaçãoDeElementosDuplicados.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class VerificaçãoDeElementosDuplicados {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] sec = sc.nextLine().split(" ");
+		int[] numSec = toArrayInt(sec);
+		System.out.println(duplicados(numSec));
+		sc.close();
+		
+	}
+	
+	private static boolean duplicados(int[] v) {
+		boolean retorno = false;
+		int i = 0;
+		while(i < v.length && retorno == false) {
+			int j = i +1;
+			while(j < v.length && retorno == false) {
+				if(v[i] == v[j]) {
+					retorno = true;
+				}else {
+					j++;
+				}
+			}
+			i++;
+		}
+		return retorno;
+	}
+
+	private static int[] toArrayInt(String[] sec) {
+		int[] numSec = new int[sec.length];
+		for(int i = 0; i < sec.length; i++) {
+			numSec[i] = Integer.parseInt(sec[i]);
+		}
+		return numSec;
+	}
+}
