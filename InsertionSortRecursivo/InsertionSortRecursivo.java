8f874a3fbdd52cb01f9503b5864d170873ded21d
Renildod7 <renildo.melo@ccc.ufcg.edu.br>
2019-08-26 13:14:33 -0300

    .

--- InsertionSortRecursivo/InsertionSortRecursivo.java
+++ InsertionSortRecursivo/InsertionSortRecursivo.java
@@ -6,26 +6,23 @@ class InsertionSortRecursivo {
 		Scanner sc = new Scanner(System.in);
 		String[] sec = sc.nextLine().split(" ");
 		int[] numSec = toArrayInt(sec);
-		insertionSort(numSec);
+		insertionSortRecursivo(numSec, 1);
 		sc.close();
 		
 	}
-
-	
-	private static void insertionSort(int[] v) {
-		for(int i = 1; i < v.length; i++) {
-			
-			int iAtual = i;
-			int iAnterior = i - 1;
-			while(iAnterior >= 0 && v[iAtual] < v[iAnterior]) {
-				int aux = v[iAtual];
-				v[iAtual] = v[iAnterior];
-				v[iAnterior] = aux;
-				iAtual--;
-				iAnterior--;
+	private static void insertionSortRecursivo(int[] v, int i) {
+			if(i < v.length) {
+				
+				int key = v[i];
+				int j = i - 1;
+				while(j >= 0 && key < v[j]) {
+					v[j + 1] = v[j];
+					v[j] = key;
+					j--;
+				}
+				System.out.println(Arrays.toString(v));
+				insertionSortRecursivo(v, i + 1);
 			}
-			System.out.println(Arrays.toString(v));
-		}
 	}
 	
 	private static int[] toArrayInt(String[] sec) {
@@ -34,5 +31,19 @@ class InsertionSortRecursivo {
 			numSec[i] = Integer.parseInt(sec[i]);
 		}
 		return numSec;
+	}
+	
+	private static void insertionSort(int[] v, int esquerda, int direita) {
+		for(int i = esquerda; i < direita +1; i++) {
+			
+			int key = v[esquerda];
+			int j = i - 1;
+			while(j >= esquerda && key < v[j]) {
+				v[j + 1] = v[j];
+				v[j] = key;
+				j--;
+			}
+			System.out.println(Arrays.toString(v));
+		}
 	}
 }
